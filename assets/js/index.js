f2a1d72c1b2beeb6c089a42ade16d90021b5423f
Juan Alberto <jaranedakoning@gmail.com>
2024-05-09 17:47:59 -0400

    prueba módulo 2 Viajes Chile

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $("#frmContact").on("submit", (e) => {
+        e.preventDefault();
+        let mensaje = "";
+        let alerta = true;
+
+        let nombre = $("#name").val();
+        let subject = $("#subject").val();
+        let message = $("#message").val();
+
+        if (nombre.length < 1) {
+            mensaje += "- Debe ingresar un Nombre\n";
+            alerta = false;
+        }
+if (subject.length < 1) {
+            mensaje += "- Debe ingresar un Asunto\n";
+            alerta = false;
+        }
+
+        if (message.length < 1) {
+            mensaje += "- Debe ingresar un Mensaje\n";
+            alerta = false;
+        }
+
+        if (alerta) {
+            alert("Mensaje enviado Sr/Sra " + $("#name").val() + "!"); 
+            $("#name").val("");
+            $("#subject").val("");
+            $("#message").val("");
+        } else {
+            alert(mensaje);
+        }
+    });
+
+    $("input, textarea").tooltip();
+});
